23f8c817f2c072eaa547b628314cdd10913caf76
Yichuan Shen <shen.yichuan@gmail.com>
2017-09-25 17:01:06 +0200

    Make website responsive

--- _build/Page.js
+++ _build/Page.js
@@ -5,8 +5,9 @@ module.exports = class Page extends Component {
         return h('html', {},
             h('head', {},
                 h('meta', {charset: 'utf-8'}),
-                this.props.stylesheet && h('link', {rel: 'stylesheet', href: this.props.stylesheet}),
-                this.props.title && h('title', {}, this.props.title)
+                h('meta', {name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'}),
+                this.props.title && h('title', {}, this.props.title),
+                this.props.stylesheet && h('link', {rel: 'stylesheet', href: this.props.stylesheet})
             ),
             h('body', this.props.bodyProps,
                 this.props.children
